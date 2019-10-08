914f867f56ba563403653d51cfca25ab363c7e41
Alisha Stilwell <alishagyt@gmail.com>
2019-10-08 19:34:26 -0400

    Display list of movies and customers

--- Controllers/MoviesController.cs
+++ Controllers/MoviesController.cs
@@ -11,23 +11,30 @@ namespace VidlyApplication.Controllers
 {
     public class MoviesController : Controller
     {
-        // GET: Movies
+        public ViewResult Index()
+        {
+            var movies = GetMovies();
+
+            return View(movies);
+        }
+
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie { Id = 1, Name = "Shrek" },
+                new Movie { Id = 2, Name = "Wall-e" }
+            };
+        }
+
+        // GET: Movies/Random
         public ActionResult Random()
         {
-            var movie = new Movie()
-            {
-                Name = "Shrek!"
-            };
+            var movie = new Movie() { Name = "Shrek!" };
             var customers = new List<Customer>
             {
-                new Customer
-                {
-                    Name = "Customer1"
-                },
-                new Customer
-                {
-                    Name = "Customer2"
-                }
+                new Customer { Name = "Customer 1" },
+                new Customer { Name = "Customer 2" }
             };
 
             var viewModel = new RandomMovieViewModel
@@ -37,33 +44,6 @@ namespace VidlyApplication.Controllers
             };
 
             return View(viewModel);
-        }
-
-        public ActionResult Edit(int id)
-        {
-            return Content("id=" + id);
-        }
-
-        public ActionResult Index(int? pageIndex, string sortBy)
-        {
-            if (!pageIndex.HasValue)
-            {
-                pageIndex = 1;
-            }
-
-            if (String.IsNullOrWhiteSpace(sortBy))
-            {
-                sortBy = "Name";
-            }
-
-            return Content($"pageIndex={pageIndex}&sortBy={sortBy}");
-        }
-
-        [Route("movies/released/{year:regex(//d{4})}/{month:regex(\\d{2}):range(1, 12)}")]
-        public ActionResult ByReleaseDate(int year, int month)
-        {
-
-            return Content(year + "/" + month);
         }
     }
 }
